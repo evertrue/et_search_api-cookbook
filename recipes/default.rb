8f526d2a9efef2fec7d3cdb3272c4516262d8c71
Jeff Byrnes <jeff@evertrue.com>
2013-11-20 18:18:50 -0500

    Rubocop cleanup

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,24 +6,24 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-include_recipe "et_hipsnip-jetty::default"
-include_recipe "et_newrelic"
-include_recipe "et_users::evertrue"
+include_recipe 'et_hipsnip-jetty::default'
+include_recipe 'et_newrelic'
+include_recipe 'et_users::evertrue'
 
 # creds = Chef::EncryptedDataBagItem.load(
 #     'secrets',
 #     'api_keys'
 #   )['autodeploy'][node.chef_environment]
 
-# autodeployer "search.war" do
-#   webapp_dir "/usr/share/jetty/webapps"
-#   job "search-XXXXXXXXXXX"
-#   local_user "deploy"
-#   ci_url "https://ci.evertrue.com"
+# autodeployer 'search.war' do
+#   webapp_dir '/usr/share/jetty/webapps'
+#   job 'search-XXXXXXXXXXX'
+#   local_user 'deploy'
+#   ci_url 'https://ci.evertrue.com'
 #   ci_user creds['ci']['user']
 #   ci_password creds['ci']['password']
 #   db_user creds['db']['user']
 #   db_password creds['db']['password']
-#   db_host "stage-etdb.cg0lvth7azzh.us-east-1.rds.amazonaws.com"
-#   db_schema "deployer"
+#   db_host 'stage-etdb.cg0lvth7azzh.us-east-1.rds.amazonaws.com'
+#   db_schema 'deployer'
 # end
