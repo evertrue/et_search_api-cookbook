5714179e7d7d1fc2d3016e0be4fbdada750b9fd1
Jeff Byrnes <jeff@evertrue.com>
2013-11-21 11:12:48 -0500

    Bump to v.1.3.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
-## 1.0.1:
+## 1.3.2
+
+* Disable a data bag request, as it is unused (no autodeployer yet)
+* Clean up code style as per Rubocop
+* Add missing dependency to metadata, as per Foodcritic
+* Update & expand the README
+
+## 1.3.1:
 
 * Initial release of et_search_api
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Wrapper cookbook to support EverTrue Search API'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.3.1'
+version          '1.3.2'
 
 depends         'et_hipsnip-jetty', '= 1.3.0'
 depends         'et_newrelic'
