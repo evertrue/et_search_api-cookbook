cdb9c14c7bfd1a82a07c049df6a0f4d0b0a6a2f4
Jeff Byrnes <jeff@evertrue.com>
2013-12-19 14:58:43 -0500

    Update et_hipsnip-jetty to 1.3.1

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Wrapper cookbook to support EverTrue Search API'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.3.2'
 
-depends         'et_hipsnip-jetty', '= 1.3.0'
+depends         'et_hipsnip-jetty', '= 1.3.1'
 depends         'et_users'
 depends         'autodeployer', '= 1.0.2'
