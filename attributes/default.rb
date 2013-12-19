2807b21fcfae1c803bc217bd6b3d30f040ab5242
Jeff Byrnes <jeff@evertrue.com>
2013-12-19 17:11:45 -0500

    Remove New Relic Java Agent from Jetty options

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['cluster']['role'] = 'app'
 default['cluster']['name'] = 'api'
 
-set['jetty']['java_options'] = "-Xmx1G -Djava.awt.headless=true -Dcom.et.env=#{node.chef_environment} -javaagent:#{node['newrelic']['agent_home']}/newrelic.jar"
+set['jetty']['java_options'] = "-Xmx1G -Djava.awt.headless=true -Dcom.et.env=#{node.chef_environment}"
