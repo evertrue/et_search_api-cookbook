4de8922fe04cbe2967f0ce255396712fe1252702
Jeff Byrnes <jeff@evertrue.com>
2014-04-18 12:23:21 -0400

    Update et_hipsnip-jetty to 2.0.1 to decrease maxThreads

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Wrapper cookbook to support EverTrue Search API'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.4.1'
 
-depends         'et_hipsnip-jetty', '= 1.3.1'
+depends         'et_hipsnip-jetty', '= 2.0.1'
 depends         'et_users', '>= 1.1.1'
 depends         'autodeployer', '= 1.0.2'
