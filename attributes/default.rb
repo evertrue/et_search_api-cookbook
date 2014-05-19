520d4dd8b413e5248ec82f9ff6e661db3bfe0004
Eric Herot <eric.github@herot.com>
2014-05-19 17:15:55 -0400

    Linting cleanup

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 default['cluster']['role'] = 'app'
 default['cluster']['name'] = 'api'
 
-set['jetty']['java_options'] = "-Xmx1G -Djava.awt.headless=true -Dcom.et.env=#{node.chef_environment}"
+set['jetty']['java_options'] = '-Xmx1G -Djava.awt.headless=true ' \
+  "-Dcom.et.env=#{node.chef_environment}"
