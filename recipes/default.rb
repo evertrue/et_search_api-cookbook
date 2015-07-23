42d84f27dfc77b0efbc13aad4f5a992d90c9408b
Ayush Sangani <ayushsangani@gmail.com>
2015-07-23 11:44:35 -0400

    Include storage recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,19 @@
 #
 include_recipe 'et_hipsnip-jetty::default'
 include_recipe 'et_users::evertrue'
+include_recipe 'storage'
+
+if node['storage']['ephemeral_mounts']
+  node.set['jetty']['app_log_dir'] =
+    "#{node['storage']['ephemeral_mounts'].first}/application_logs"
+else
+  node.set['jetty']['app_log_dir'] = '/var/log/application_logs'
+end
+
+
+directory node['jetty']['app_log_dir'] do
+  owner  node['jetty']['user']
+  group  node['jetty']['group']
+  mode   0775
+  action :create
+end
