a739f2f74a709564bd510de4fa129ceb314035d2
Ayush Sangani <ayushsangani@gmail.com>
2015-07-23 14:21:47 -0400

    Bump to v1.5.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@ CHANGELOG for et_contacts_api
 ===================================
 This file is used to list changes made in each version of the et_contacts_api cookbook.
 
+v1.5.4 (2015-07-23)
+-------------------
+- Set jetty java options after storage recipe
+
 v1.5.3 (2015-07-23)
 -------------------
 - Fix spacing issue in recipe
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Wrapper cookbook to support EverTrue Search API'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.5.3'
+version          '1.5.4'
 
 depends         'et_hipsnip-jetty', '= 2.1.0'
 depends         'et_users', '>= 1.4.1'
