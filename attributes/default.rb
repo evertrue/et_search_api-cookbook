f95be71bc6a76d3453b2c0d3f835348ebb9a70b3
Ayush Sangani <ayushsangani@gmail.com>
2015-07-23 13:35:08 -0400

    Fix spacing issue

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,5 @@ default['cluster']['role'] = 'app'
 default['cluster']['name'] = 'api'
 
 set['jetty']['java_options'] = '-Xmx1G -Djava.awt.headless=true ' \
-  "-Dcom.et.env=#{node.chef_environment}" \
+  "-Dcom.et.env=#{node.chef_environment} " \
   "-Dcom.et.jetty.log.dir=#{node['jetty']['app_log_dir']}"
